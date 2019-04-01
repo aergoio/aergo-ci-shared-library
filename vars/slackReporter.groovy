839cabd6312a98b9a3f5dc4e3b2264ef01ec8419
asteroid <asteroidable@gmail.com>
2019-04-01 22:19:40 +0900

    update slackReporter.groovy

--- vars/slackReporter.groovy
+++ vars/slackReporter.groovy
@@ -4,8 +4,12 @@ import hudson.tasks.test.AbstractTestResultAction
 
 @NonCPS
 def call(def currentBuild, def channel='') {
-  def buildResult = currentBuild.currentResult
+  def buildResult = currentBuild.result
+  if (!buildResult) {
+    buildResult = currentBuild.currentResult
+  }
   def COLOR_MAP = ['SUCCESS': 'good', 'UNSTABLE': 'warning', 'FAILURE': 'danger', 'ABORTED': 'danger']
+  def color = COLOR_MAP[buildResult]
   def msg = "<${env.JOB_URL}|${env.JOB_NAME}> - Build <${env.BUILD_URL}|#${env.BUILD_NUMBER}>: ${buildResult}"
   AbstractTestResultAction testResults =  currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
   if (testResults != null) {
@@ -15,9 +19,9 @@ def call(def currentBuild, def channel='') {
     def passed = total - failed - skipped
     msg += "\n  Test Status: Total ${total}, Passed ${passed}, Failed ${failed}, Skipped ${skipped}"
   }
-  if (channel != '') {
-    slackSend message: msg, color: COLOR_MAP[buildResult], channel: channel
+  if (!channel) {
+    slackSend message: msg, color: color
   } else {
-    slackSend message: msg, color: COLOR_MAP[buildResult]
+    slackSend message: msg, color: color, channel: channel
   }
 }
